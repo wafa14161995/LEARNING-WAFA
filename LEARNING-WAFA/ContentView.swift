24b00f5d0f700d2e447354c74b57df3442510b0a
wafa14161995 <Wafa14161995@gmail.com>
2024-10-20 16:09:17 +0300

    Initial Commit

---
+++ LEARNING-WAFA/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LEARNING-WAFA
+//
+//  Created by Wafa Awad  on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LEARNING-WAFA/LEARNING_WAFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEARNING_WAFAApp.swift
+//  LEARNING-WAFA
+//
+//  Created by Wafa Awad  on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LEARNING_WAFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
